a20566b3f57425f1ee94d4a111df12b976564216
Nathan Daniel Peterson <natperson@gmail.com>
2017-11-21 16:06:45 -0800

    add some classes

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
 let yargs = require('yargs')
 let chalk = require('chalk')
 
-console.log(chalk.red.bold(yargs.argv['_']))
+// console.log(chalk.red.bold(yargs.argv['_']))
 
-class rock {
-    constructor(){
+let input = yargs.argv['_'][0]
 
+let newGameMessage = `Welcome to RoShamBo`
+
+class Rock {
+    constructor(rival){
+        console.log(this.rival)
+    }
+    battle(rival){
+        this.rival = rival
+        if(this.rival === 'paper') return true
+        if(this.rival === 'scissors') return false
     }
 }
+
+class Paper {
+    constructor(rival){
+        
+    }
+}
+
+class Scissors {
+    constructor(rival){
+        
+    }
+}
+
+function computerPlay(){
+
+}
+
+// console.log(chalk.magenta`Welcome to RoShamBo`)
+// console.log(`You played ${input}!`)
+// console.log(`The computer played scissors!`)
+// console.log(`You lost...`)
